9f8c24794d0748813fe9cedcad9def23f6ac8ddb
Tim Gortworst <tim.gortworst@gmail.com>
2022-10-21 13:43:00 +0100

    added RepoListFragment.kt and wired navigation component

--- app/src/main/java/com/github/abnamro/presentation/repo/RepoActivity.kt
+++ app/src/main/java/com/github/abnamro/presentation/repo/RepoActivity.kt
@@ -2,13 +2,29 @@ package com.github.abnamro.presentation.repo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.github.abnamro.R
+import com.github.abnamro.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RepoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val navController: NavController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        (navHostFragment as NavHostFragment).navController
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // TODO
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupActionBarWithNavController(navController, AppBarConfiguration(navController.graph))
     }
+
+    override fun onSupportNavigateUp() =
+        navController.navigateUp() || super.onSupportNavigateUp()
 }
