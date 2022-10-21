50af87aef6947e3c2fa7a8e163c32723a8e190d7
Tim Gortworst <tim.gortworst@gmail.com>
2022-10-21 13:25:00 +0100

    added styling resources and initial Application and Activity entry point

---
+++ app/src/main/java/com/github/abnamro/presentation/AbnAmroApp.kt
@@ -0,0 +1,7 @@
+package com.github.abnamro.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AbnAmroApp : Application()
---
+++ app/src/main/java/com/github/abnamro/presentation/repo/RepoActivity.kt
@@ -0,0 +1,14 @@
+package com.github.abnamro.presentation.repo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RepoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO
+    }
+}
